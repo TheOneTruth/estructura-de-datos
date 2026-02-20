dd8bd5ef8d9556d33ba8d0c3c7163440ecb37ecf
TheOneTruth <luiferrojas61@gmail.com>
2026-02-20 07:34:36 -0500

    Se termina el prog de sistema academico

--- sistema-academico-arrays/src/Main.java
+++ sistema-academico-arrays/src/Main.java
@@ -6,20 +6,53 @@ public class Main {
         double notas[] = new double[10];
 
         //Asignamos valores a cada posicion
-        for (int i=0; i < notas.length; i++){
-            System.out.println("Ingrese la nota del estudiante" + i);
-           notas[i] = input.nextDouble();
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Ingrese la nota del estudiante " + (i + 1));
+            notas[i] = input.nextDouble();
 
         }
-        System.out.println("El promedio de las notas es:" + promedio(notas));
+        System.out.println("El promedio de las notas es: " + promedio(notas));
+        System.out.println("La nota maxima del curso es: " + notaMaxima(notas));
+        System.out.println("La nota mminima del curso es: " + notaMinima(notas));
+        System.out.println("Cantidad de estudiantes que aprobaron: " + cantidadAprobados(notas));
 
     }
 
     public static double promedio(double notas[]) {
         double suma = 0;
-        for (int i = 0; i < notas.length; i++){
+        for (int i = 0; i < notas.length; i++) {
             suma += notas[i];
         }
-        return  suma / notas.length;
+        return suma / notas.length;
+    }
+
+    public static double notaMaxima(double notas[]) {
+        double notaMax = 0;
+        for (double nota : notas) {
+            if (nota > notaMax) {
+                notaMax = nota;
+            }
+        }
+        return notaMax;
+    }
+
+    public static double notaMinima(double notas[]) {
+        double notaMin = 5;
+        for (double nota : notas) {
+            if (nota < notaMin) {
+                notaMin = nota;
+            }
+        }
+        return notaMin;
+    }
+
+    public static byte cantidadAprobados(double notas[]) {
+        byte cant = 0;
+        for (double nota : notas) {
+            if (nota >= 3) {
+                cant++;
+            }
+        }
+        return cant;
     }
 }
